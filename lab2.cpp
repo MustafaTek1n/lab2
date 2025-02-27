5caa86dc7a9a1340e3d04e9a7bc349a6fc3ddc7d
MUSTAFA TEKİN <152206620+MustafaTek1n@users.noreply.github.com>
2025-02-27 13:45:29 +0300

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int deger ;
+	cout<<"enter a number between 2-9 : ";
+	cin>>deger;
+	int i=0;
+	
+	while(deger>i){
+	    cout<<"*";
+	    i++;
+	}
+	return 0;
+}
